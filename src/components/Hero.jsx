471ccc4e693ee2ddf3258e6b20b6ea0c51b68c74
JohnMarkCuya <markcuya017@gmail.com>
2025-10-13 16:41:23 +0800

    Initial commit

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -16,7 +16,7 @@ const Hero = () => {
 
         <div>
           <h1 className={`${styles.heroHeadText} text-white`}>
-            Hi, I'm <span className='text-[#915EFF]'>John Mark</span>
+            Hi, I'm <span className='text-[#915EFF]'>Mark</span>
           </h1>
           <p className={`${styles.heroSubText} mt-2 text-white-100`}>
             I build websites and help people <br className='sm:block hidden' />
