0f923a091717f299107f419f7184f589cbb258da
JohnMarkCuya <markcuya017@gmail.com>
2025-10-19 16:31:09 +0800

    Initial commit

--- src/components/canvas/Ball.jsx
+++ src/components/canvas/Ball.jsx
@@ -1,4 +1,4 @@
-import React, { Suspense, useEffect, useState } from "react";
+import React, { Suspense } from "react";
 import { Canvas } from "@react-three/fiber";
 import {
   Decal,
@@ -7,35 +7,23 @@ import {
   Preload,
   useTexture,
 } from "@react-three/drei";
+
 import CanvasLoader from "../Loader";
 
-const Ball = ({ imgUrl, isMobile }) => {
-  const [decal] = useTexture([imgUrl]);
+const Ball = (props) => {
+  const [decal] = useTexture([props.imgUrl]);
 
   return (
-    <Float
-      speed={isMobile ? 1.2 : 1.75}
-      rotationIntensity={isMobile ? 0.7 : 1}
-      floatIntensity={isMobile ? 1.3 : 2}
-    >
-      {/* ✅ Safe lighting for both mobile and desktop */}
-      <ambientLight intensity={isMobile ? 0.2 : 0.25} />
-      <directionalLight position={[0, 0, 0.05]} intensity={isMobile ? 0.9 : 1} />
-
-      <mesh
-        castShadow={!isMobile}
-        receiveShadow={!isMobile}
-        scale={2.75}
-        dispose={null}
-      >
+    <Float speed={1.75} rotationIntensity={1} floatIntensity={2}>
+      <ambientLight intensity={0.25} />
+      <directionalLight position={[0, 0, 0.05]} />
+      <mesh castShadow receiveShadow scale={2.75}>
         <icosahedronGeometry args={[1, 1]} />
         <meshStandardMaterial
-          color="#fff8eb"
+          color='#fff8eb'
           polygonOffset
           polygonOffsetFactor={-5}
           flatShading
-          roughness={isMobile ? 0.8 : 0.6}
-          metalness={isMobile ? 0 : 0.3}
         />
         <Decal
           position={[0, 0, 1]}
@@ -50,31 +38,18 @@ const Ball = ({ imgUrl, isMobile }) => {
 };
 
 const BallCanvas = ({ icon }) => {
-  const [isMobile, setIsMobile] = useState(false);
-
-  useEffect(() => {
-    const mediaQuery = window.matchMedia("(max-width: 500px)");
-    const update = () => setIsMobile(mediaQuery.matches);
-    update();
-    mediaQuery.addEventListener("change", update);
-    return () => mediaQuery.removeEventListener("change", update);
-  }, []);
-
   return (
     <Canvas
-      frameloop="demand"
-      dpr={[1, 1.5]}
-      gl={{
-        preserveDrawingBuffer: true,
-        powerPreference: isMobile ? "low-power" : "high-performance",
-      }}
-      style={{ touchAction: "none" }}
+      frameloop='demand'
+      dpr={[1, 2]}
+      gl={{ preserveDrawingBuffer: true }}
     >
       <Suspense fallback={<CanvasLoader />}>
         <OrbitControls enableZoom={false} />
-        <Ball imgUrl={icon} isMobile={isMobile} />
-        <Preload all />
+        <Ball imgUrl={icon} />
       </Suspense>
+
+      <Preload all />
     </Canvas>
   );
 };
