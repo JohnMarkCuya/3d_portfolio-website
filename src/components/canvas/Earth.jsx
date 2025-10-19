e180cf41a90577ae91a303c6fd8686a6174cce2a
JohnMarkCuya <markcuya017@gmail.com>
2025-10-19 16:49:20 +0800

    No Earth on Mobile Devices

--- src/components/canvas/Earth.jsx
+++ src/components/canvas/Earth.jsx
@@ -1,22 +1,38 @@
-import React, { Suspense } from "react";
+import React, { Suspense, useEffect, useState } from "react"; // 🟢 added useEffect, useState
 import { Canvas } from "@react-three/fiber";
 import { OrbitControls, Preload, useGLTF } from "@react-three/drei";
-
 import CanvasLoader from "../Loader";
 
 const Earth = () => {
   const earth = useGLTF("./planet/scene.gltf");
-
   return (
     <primitive object={earth.scene} scale={2.5} position-y={0} rotation-y={0} />
   );
 };
 
 const EarthCanvas = () => {
+  // 🟢 Add this: detect mobile screen size
+  const [isMobile, setIsMobile] = useState(false);
+
+  useEffect(() => {
+    const mediaQuery = window.matchMedia("(max-width: 500px)");
+
+    const handleChange = (e) => setIsMobile(e.matches);
+    handleChange(mediaQuery); // initial check
+    mediaQuery.addEventListener("change", handleChange);
+
+    return () => mediaQuery.removeEventListener("change", handleChange);
+  }, []);
+
+  // 🟢 If mobile, skip rendering the 3D Earth entirely
+  if (isMobile) {
+    return null;
+  }
+
   return (
     <Canvas
       shadows
-      frameloop='demand'
+      frameloop="demand"
       dpr={[1, 2]}
       gl={{ preserveDrawingBuffer: true }}
       camera={{
@@ -34,7 +50,6 @@ const EarthCanvas = () => {
           minPolarAngle={Math.PI / 2}
         />
         <Earth />
-
         <Preload all />
       </Suspense>
     </Canvas>
